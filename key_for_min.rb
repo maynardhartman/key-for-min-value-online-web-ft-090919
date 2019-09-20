6f99e8fad8a78dc131022a0dd0547056741b546b
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-20 06:56:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   name_hash.collect { |name, value|
     ammount = value
     next
-  
+    if name_hash["name"] == {}
+      return nill
     if ammount < value
        ammount = value
     end
